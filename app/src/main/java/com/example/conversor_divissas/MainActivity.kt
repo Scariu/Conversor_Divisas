c29f62ffb9ee07d4f7b3d7fee4a36b064aca3ea9
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-20 20:45:59 -0400

    Se implementa lista de divisas en spinners.

--- app/src/main/java/com/example/conversor_divissas/MainActivity.kt
+++ app/src/main/java/com/example/conversor_divissas/MainActivity.kt
@@ -2,13 +2,23 @@ package com.example.conversor_divissas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.conversor_divissas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private  val divisas = listOf<String>("USD", "CLP", "EURO") //Se crea lista
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, divisas)//Se crea adapter
+        binding.spinnerMoneda1.adapter = adapter
+        binding.spinnerMoneda2.adapter = adapter
+        /*
+        *Tambien se puede declarar así
+        * binding.spinnerMoneda1.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, divisas)
+        * binding.spinnerMoneda1.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, divisas)
+        */
     }
 }
